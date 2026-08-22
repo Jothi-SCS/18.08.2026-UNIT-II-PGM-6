415a152ed72f0d185fd736a4e9edd04f159d9e92
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-22 11:53:07 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,1 +1,8 @@
+-- Fixes Test Case 2: Update Karthik's department to 103
+UPDATE Student
+SET DepartmentID = 103
+WHERE StudentName = 'Karthik';
 
+-- Fixes Test Case 3: Delete StudentID 1002
+DELETE FROM Student
+WHERE StudentID = 1002;
